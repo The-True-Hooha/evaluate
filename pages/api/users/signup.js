21c9c9dc6e3764c0ff34d9b0e5c637e3204daa4e
David Ogar <owogogahhero@outlook.com>
2022-10-31 11:22:45 +0100

    feat: add route for user signup

--- pages/api/users/signup.js
+++ pages/api/users/signup.js
@@ -8,12 +8,50 @@ export default async function handler(req, res) {
     const {username, email, password} = req.body;
 
     // validator for input field
-    if(!username || email || password){
+    // no field in the schema for first name and last name
+    if(!username || !email || !password){
         res.status(400).json({
             message: "missing field, please make sure all inputs are field correctly"
         });
         return;
     }
-
     // todo: change useremail to email and update prisma schema
+
+    prisma.user.findUnique({
+        where: {
+            useremail: useremail
+        }
+    }).then(user => {
+        if(user) {
+            res.status(400).json({
+                message: "email already taken"
+            });
+        } else {
+            prisma.user.findUnique({
+                where: {
+                    password: password
+                }
+            }).then(user => {
+                if(user) {
+                    res.status(400).json({
+                        message: "password has previously been used already"
+                    });
+                } else{
+                    prisma.user.create({
+                        data: {
+                            useremail: useremail,
+                            username: username,
+                            password: password
+                        }
+                    }).then(user => {
+                        res.status(201).json({
+                            message: "account created successfully",
+                            user: user
+                        })
+                        console.log(user)
+                    });
+                }
+            });
+        }
+    });
 }
