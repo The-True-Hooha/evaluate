b3cef7ce4fd14d041344a5a58d4480ddacb27cad
David Ogar <owogogahhero@outlook.com>
2022-11-01 16:18:40 +0100

    add logout route

---
+++ pages/api/users/logout.js
@@ -0,0 +1,31 @@
+// import { PrismaClient } from ".prisma/client";
+import { serialize } from "cookie";
+
+
+export default async function handler(req, res){
+    // const prisma = new PrismaClient;
+
+    const { cookies } = req;
+    const jwt = cookies.evaluate;
+
+    if(!jwt){
+        return res.json({
+            message: "user not logged in..."
+        })
+    } else{
+        const serialized = serialize("evaluate", null, {
+            httpOnly: true,
+            sameSite: "strict",
+            secure: process.env.PHASE,
+            maxAge: -1,
+            path: "/"
+        });
+
+        res.setHeader("Set-Cookie", serialized);
+
+        res.status(200).json({
+            message: "successfully logged out"
+        })
+    }
+
+}
