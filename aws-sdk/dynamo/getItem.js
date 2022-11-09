f9649d4f4e286496700fee7ff32f6672e47644ca
Tochi <tochey@outlook.com>
2022-11-09 10:58:59 -0500

    main rce component completed

---
+++ aws-sdk/dynamo/getItem.js
@@ -0,0 +1,14 @@
+import { GetItemCommand } from "@aws-sdk/client-dynamodb";
+import { client } from '../config/dynamo.config'
+export default async (jobId) => {
+    const input = {
+        TableName: "evaluateCacheStore",
+        Key: { "jobId" : {"S" : jobId} }
+    }
+
+    const command = new GetItemCommand(input);
+    const res = client.send(command)
+    return res
+}
+
+
