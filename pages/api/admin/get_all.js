63a044f6200c942bbc8f2ded2126ba6a56094b3e
David Ogar <owogogahhero@outlook.com>
2022-11-14 16:58:08 +0100

    feat: auth || permission

--- pages/api/admin/get_all.js
+++ pages/api/admin/get_all.js
@@ -1,31 +1,38 @@
-import prisma from "../../../config/prisma.connect"
+import {prisma, Role} from "../../../config/prisma.connect"
 
-export default async function handler(req, res) {
+export default async function handler(req, res, next) {
 
-  try {
-    const getAll = await prisma.user.findMany({
-      where: {
-        isEnabled: false,
-      },
+  if(Role !== "FACULTY"){
+    res.status(401).json({
+      message: "error, you are not allowed to access this data"
+    })
 
-      orderBy: {
-        email: "asc",
-        // best use case is to orderBy createdAt(Date/Time)
-        //i agree
-      },
-    });
-    if (getAll) {
-      res.status(200).json({
-        message: "returned all users awaiting access",
-        data: getAll.map((v) => ({
-          username: v.username,
-          email: v.email,
-        })),
+    // automatically redirects to homepage or login
+  } else{
+    try {
+      const getAll = await prisma.user.findMany({
+        where: {
+          isEnabled: false,
+        },
+        orderBy: {
+          email: "asc",
+          // best use case is to orderBy createdAt(Date/Time)
+          //i agree
+        },
+      });
+      if (getAll) {
+        res.status(200).json({
+          message: "returned all users awaiting access",
+          data: getAll.map((v) => ({
+            username: v.username,
+            email: v.email,
+          })),
+        });
+      }
+    } catch (error) {
+      res.status(404).json({
+        message: `invalid request ${error}`,
       });
     }
-  } catch (error) {
-    res.status(404).json({
-      message: `invalid request ${error}`,
-    });
-  }
+  }  
 }
