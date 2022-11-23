7348e649b77063ab1a26dc442d13f40641075dd9
David Ogar <owogogahhero@outlook.com>
2022-11-23 13:54:55 +0100

    refactor: chang tailwind class tag

--- src/pages/login.js
+++ src/pages/login.js
@@ -32,33 +32,33 @@ export default function Login() {
     }
 
     return (
-        <section class='gradient-form h-full bg-gray-200 md:h-screen'>
-            <div class='container h-full py-12 px-6'>
-                <div class='g-6 flex h-full flex-wrap items-center justify-center text-gray-800'>
-                    <div class='xl:w-10/12'>
-                        <div class='block rounded-lg bg-white shadow-lg'>
-                            <div class='g-0 lg:flex lg:flex-wrap'>
-                                <div class='px-4 md:px-0 lg:w-6/12'>
-                                    <div class='md:mx-6 md:p-12'>
-                                        <div class='text-center'>
+        <section className='gradient-form h-full bg-gray-200 md:h-screen'>
+            <div className='container h-full py-12 px-6'>
+                <div className='g-6 flex h-full flex-wrap items-center justify-center text-gray-800'>
+                    <div className='xl:w-10/12'>
+                        <div className='block rounded-lg bg-white shadow-lg'>
+                            <div className='g-0 lg:flex lg:flex-wrap'>
+                                <div className='px-4 md:px-0 lg:w-6/12'>
+                                    <div className='md:mx-6 md:p-12'>
+                                        <div className='text-center'>
                                             <img
-                                                class='mx-auto w-48'
+                                                className='mx-auto w-48'
                                                 src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp'
                                                 alt='logo'
                                             />
-                                            <h4 class='mt-1 mb-12 pb-1 text-xl font-semibold'>
+                                            <h4 className='mt-1 mb-12 pb-1 text-xl font-semibold'>
                                                 Welcome to Evaluate
                                             </h4>
                                         </div>
                                         <form>
-                                            <p class='mb-4'>
+                                            <p className='mb-4'>
                                                 Please login to your account
                                             </p>
-                                            <div class='mb-4'>
+                                            <div className='mb-4'>
                                                 <input
                                                     type='text'
                                                     name='email'
-                                                    class='form-control m-0 block w-full rounded border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-1.5 text-base font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none'
+                                                    className='form-control m-0 block w-full rounded border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-1.5 text-base font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none'
                                                     id='exampleFormControlInput1'
                                                     placeholder='Enter your SSU email'
                                                     onChange={(e) =>
@@ -66,11 +66,11 @@ export default function Login() {
                                                     }
                                                 />
                                             </div>
-                                            <div class='mb-4'>
+                                            <div className='mb-4'>
                                                 <input
                                                     type='password'
                                                     name='password'
-                                                    class='form-control m-0 block w-full rounded border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-1.5 text-base font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none'
+                                                    className='form-control m-0 block w-full rounded border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-1.5 text-base font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none'
                                                     id='exampleFormControlInput2'
                                                     placeholder='Password'
                                                     onChange={(e) =>
@@ -78,9 +78,9 @@ export default function Login() {
                                                     }
                                                 />
                                             </div>
-                                            <div class='mb-12 pt-1 pb-1 text-center'>
+                                            <div className='mb-12 pt-1 pb-1 text-center'>
                                                 <button
-                                                    class='mb-3 inline-block w-full rounded px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-blue-700 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg'
+                                                    className='mb-3 inline-block w-full rounded px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-blue-700 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg'
                                                     type='button'
                                                     data-mdb-ripple='true'
                                                     data-mdb-ripple-color='light'
@@ -98,11 +98,11 @@ export default function Login() {
                                                     </span>
                                                 </p>
                                             </div>
-                                            <div class='flex items-center justify-between pb-6'>
-                                                <p class='mb-0 mr-2'></p>
+                                            <div className='flex items-center justify-between pb-6'>
+                                                <p className='mb-0 mr-2'></p>
                                                 <button
                                                     type='button'
-                                                    class='inline-block rounded border-2 border-red-600 px-6 py-2 text-xs font-medium uppercase leading-tight text-red-600 transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0'
+                                                    className='inline-block rounded border-2 border-red-600 px-6 py-2 text-xs font-medium uppercase leading-tight text-red-600 transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0'
                                                     data-mdb-ripple='true'
                                                     data-mdb-ripple-color='light'>
                                                     Danger
@@ -112,16 +112,16 @@ export default function Login() {
                                     </div>
                                 </div>
                                 <div
-                                    class='flex items-center rounded-b-lg lg:w-6/12 lg:rounded-r-lg lg:rounded-bl-none'
+                                    className='flex items-center rounded-b-lg lg:w-6/12 lg:rounded-r-lg lg:rounded-bl-none'
                                     style={{
                                         background:
                                             "linear-gradient( to right,#ee7724, #d8363a,#dd3675, #b44593 )",
                                     }}>
-                                    <div class='px-4 py-6 text-white md:mx-6 md:p-12'>
-                                        <h4 class='mb-6 text-xl font-semibold'>
+                                    <div className='px-4 py-6 text-white md:mx-6 md:p-12'>
+                                        <h4 className='mb-6 text-xl font-semibold'>
                                             Smart and Intuitive grading platform
                                         </h4>
-                                        <p class='text-sm'>
+                                        <p className='text-sm'>
                                             Lorem ipsum dolor sit amet,
                                             consectetur adipisicing elit, sed do
                                             eiusmod tempor incididunt ut labore
