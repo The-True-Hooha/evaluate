4e1b18bd7e52411e5337e675d3a92b9798a8d707
David Ogar <owogogahhero@outlook.com>
2022-11-24 13:28:27 +0100

    refactor: minor changes

--- src/pages/student/courses/[courseId]/activity/[activityId].js
+++ src/pages/student/courses/[courseId]/activity/[activityId].js
@@ -1,13 +1,9 @@
-import Head from "next/head"
 import CodeUi from "../../../../../components/codeMirror"
 
 export default function StudentActivity() {
     return (
         <div className='App'>
-            <Head>
                 <title>evaluate</title>
-                <meta name='description' content='web editor based rce' />
-            </Head>
             <div className='flex justify-center'>
                 <CodeUi />
             </div>
