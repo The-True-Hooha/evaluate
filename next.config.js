16004c38a158f7673858003e672eea89114db1a9
DON <don@DONs-Laptop.local>
2022-12-04 22:15:32 -0500

    updated produrl

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-    basePath: '/main',
     baseUrl: 'https://main.dn1yub99dp50a.amplifyapp.com/',
     images: {
         remotePatterns: [
