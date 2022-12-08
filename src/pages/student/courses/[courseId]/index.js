bd4911da7868a3caf6c37c279ad7ef8299df3242
DON <don@DONs-Laptop-2.local>
2022-12-08 09:12:03 -0500

    prod-test: pushing ahead oof demo

--- src/pages/student/courses/[courseId]/index.js
+++ src/pages/student/courses/[courseId]/index.js
@@ -25,7 +25,7 @@ export default function Index({ info }) {
                             available={true}
                             availableto={e.availableto}
                             activityId={e.activityId}
-                            key={e.activityId}
+                            key={index}
                         />
                     </div>
                 )
