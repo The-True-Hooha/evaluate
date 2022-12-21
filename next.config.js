b98e71b139e0b4130b133f798583364108c5b9a1
David Ogar <owogogahhero@outlook.com>
2022-12-21 16:40:10 +0100

    build: upgrade dev dependencies to support appDir

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        appDir: true,
+    },
+    images: {
+        domains: ['upload.wikimedia.org'],
+    },
     reactStrictMode: true,
     swcMinify: true,
+    
 }
 
 module.exports = nextConfig
